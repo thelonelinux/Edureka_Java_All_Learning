670fbce41baedd8649dc4240cad0177449588864
thelonelinux <vickythetechguy@gmail.com>
2024-06-17 00:51:00 +0530

    Demo Services Code by sir

---
+++ Microservices Code by Sir/Microservices_demo_services/order-service/src/main/java/demo/common/TransactionResponse.java
@@ -0,0 +1,42 @@
+package demo.common;
+
+import demo.entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionResponse 
+{
+	private Order order;
+	private String transactionId;
+	private double amount;
+	private String message;
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public String getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+}
